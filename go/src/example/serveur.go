6c0533ef604d8481d8033e93f18ddb41a455f0f0
Chomosuke123 <122450828+Chomosuke123@users.noreply.github.com>
2023-01-17 14:47:13 -0500

    Add files via upload

--- go/src/example/serveur.go
+++ go/src/example/serveur.go
@@ -3,11 +3,10 @@ package main
 import (
     "fmt"
     "net"
-    //"time"
     "strings"
     "sync"
     "reflect"
-    //"strconv"
+    "strconv"
 )
 
 func main() {
@@ -42,57 +41,92 @@ func main() {
             matrices := string(buf[:taille])
             matrices_slices := strings.Split(matrices,"/////")
             matriceA := matrices_slices[0]
-            //matriceB := matrices_slices[1]
-            //var matrice1 [2][2] int
-            //matrice2 := [][]int
-
-            fmt.Print(reflect.TypeOf(matriceA))
-            fmt.Printf("LA MATRICEA EST EGAL A : %s\n", matriceA)
-            k := 0
-            j := 0
+            matriceB := matrices_slices[1]
             matriceA_lignes := strings.Split(matriceA,"\n")
-            fmt.Print("POOP : ", len((((matriceA_lignes[1])))))
-            for i := 0; i < len(matriceA_lignes); i++ { //ATTENTION, UN STRING C EST UNE CHAINE DE BYTE ET NON PAS UNE CHAINE DE CHARACTERES (Histoire avec rune)
+            matriceB_lignes := strings.Split(matriceB,"\n")
+
+            nb_lignes_matrice1 := (len(matriceA_lignes))
+            //nb_lignes_matrice2 := (len(matriceB_lignes))
+            //nb_colonnes_matrice1 := (len(strings.Split(matriceA_lignes[0], " ")))
+            //nb_colonnes_matrice2  := (len(strings.Split(matriceB_lignes, " ")))
+
+            fmt.Print("nb_lignes : ", nb_lignes_matrice1)
+            fmt.Println(" de type : ", reflect.TypeOf(nb_lignes_matrice1))
+            tata := 2
+            matrice1 := make([][]int, tata)
+            for i := 0; i < tata; i++ {
+                matrice1[i] = make([]int, tata)
+            } //On pouvait pas passer par des arrays parce que la taille est pas constante (depend des matrices envoyées) donc ca ne marchait pas
+            matrice2 := make([][]int, tata)
+            for i := 0; i < tata; i++ {
+                matrice2[i] = make([]int, tata)
+            }
+            matriceR := make([][]int, tata)
+            for i := 0; i < tata; i++ {
+                matriceR[i] = make([]int, tata)
+            }
+
+            //fmt.Print(reflect.TypeOf(matriceA))
+            //fmt.Printf("LA MATRICEA EST EGAL A : %s\n", matriceA)
+
+            //fmt.Print("POOP : ", len((((matriceA_lignes[1])))))
+            
+            /*for i := 0; i < len(matriceA_lignes); i++ { //ATTENTION, UN STRING C EST UNE CHAINE DE BYTE ET NON PAS UNE CHAINE DE CHARACTERES (Histoire avec rune)
                 //fmt.Print("\nBLAAAAAAAAAAA :" + string((matriceA[i])))
                 matriceA_elems := strings.Split(matriceA_lignes[i], " ")
                 for j := 0; j < len(matriceA_elems); j++{
+                    matrice1[i][j],_ = strconv.Atoi((string(matriceA_elems[j])))
+                }
                     
-                }
-                if string(matriceA_lignes[i]) == " "{  // A enlever cette ligne eventuellement
-                    continue
-                }
-                if string(matriceA_lignes[i]) == "\n"{
-                    k++
-                    j=0
-                }else{
-                    //matrice1[k][j],_ = strconv.Atoi((string(matriceA_lignes[i])))
-                    j++ 
-                }
-            }
-            fmt.Println()
+            }*/
 
-            /*for i := range matrice1 {
+            for i := 0; i < 2; i++ { //ATTENTION, UN STRING C EST UNE CHAINE DE BYTE ET NON PAS UNE CHAINE DE CHARACTERES (Histoire avec rune)
+                //fmt.Print("\nBLAAAAAAAAAAA :" + string((matriceA[i])))
+                matriceA_elems := strings.Split(matriceA_lignes[i], " ")
+                for j := 0; j < 2; j++{
+                    matrice1[i][j],err = strconv.Atoi((string(matriceA_elems[j])))
+                    if err != nil {
+                        fmt.Print("ERREUR")
+                    }
+                }
+                    
+            }
+
+            fmt.Println("\nMatrice A : ")
+
+            for i := range matrice1 {
                 for j := range matrice1[i] {
                     fmt.Printf("%d ",matrice1[i][j])
                 }
                 fmt.Println()
-            }*/
-
+            }
             
+            fmt.Println("Matrice B : ")
 
-            /*lignes_matriceA := strings.Split(matriceA,"\n")
-            lignes_matriceB := strings.Split(matriceB,"\n")
-            for i,v := range lignes_matriceA{
-                fmt.Println("\nIndex : ", i, "Valeur : ", v)
+            for i := 0; i < len(matriceB_lignes); i++ { //ATTENTION, UN STRING C EST UNE CHAINE DE BYTE ET NON PAS UNE CHAINE DE CHARACTERES (Histoire avec rune)
+                //fmt.Print("\nBLAAAAAAAAAAA :" + string((matriceA[i])))
+                matriceB_elems := strings.Split(matriceB_lignes[i], " ")
+                for j := 0; j < len(matriceB_elems); j++{
+                    matrice2[i][j],_ = strconv.Atoi((string(matriceB_elems[j])))
+                }
+                    
             }
-            for i,v := range lignes_matriceB{
-                fmt.Println("\nIndex : ", i, "Valeur : ", v)
+            
+             for i := range matrice2 {
+                for j := range matrice2[i] {
+                    fmt.Printf("%d ",matrice2[i][j])
+                }
+                fmt.Println()
             }
-            colonnes_matriceB = []int
 
-            for i, v := range matrices_slices {
-                fmt.Println("\nIndex : ", i, "Valeur : ", v)
-            }*/
+            var wg sync.WaitGroup
+            for i := range matrice1 {
+                wg.Add(1)
+                go multiply(matrice1, matrice2, &matriceR, i, &wg)
+            }
+            wg.Wait()
+        
+            fmt.Println(matriceR)
             
             conn.Write([]byte("Message received.\n"))
             conn.Close()
@@ -100,11 +134,11 @@ func main() {
     }
 }
 
-func multiply(a [][]int, b [][]int, result [][]int, row int, wg *sync.WaitGroup) {
+func multiply(a [][]int, b [][]int, result *[][]int, row int, wg *sync.WaitGroup) {
     defer wg.Done()
     for col := 0; col < len(b[0]); col++ {
         for k := 0; k < len(a[0]); k++ {
-            result[row][col] += a[row][k] * b[k][col]
+            (*result)[row][col] += a[row][k] * b[k][col]
         }
     }
 }
