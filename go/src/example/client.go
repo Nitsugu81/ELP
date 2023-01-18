dfb096ba1222898dcda5f9d232e417ee298ae6de
Chomosuke123 <122450828+Chomosuke123@users.noreply.github.com>
2023-01-18 10:11:05 -0500

    Add files via upload

--- go/src/example/client.go
+++ go/src/example/client.go
@@ -5,6 +5,10 @@ import (
     "io/ioutil"
     "os"
     "net"
+    //"reflect"
+    "encoding/json"
+    "fmt"
+    "reflect"
 )
 
 const (
@@ -71,16 +75,47 @@ func main() {
                 os.Exit(1)
         }
 
-        // buffer to get matriceA
+        // Chopper la matriceR
         received := make([]byte, 1024)
-        _, err = conn.Read(received)
+        n, err := conn.Read(received)
         if err != nil {
-                println("Read matriceA failed:", err.Error())
+                println("Read matriceR failed:", err.Error())
                 os.Exit(1)
         }
+        /*matriceR := make([][]int, 3)
+        for i := 0; i < 3; i++ {
+            matriceR[i] = make([]int, 2)
+        }*/
+        var matriceR [][]int64         
+        json.Unmarshal(received[:n], &matriceR)
 
-        println("Received message:", string(received))
+        fmt.Println("Type : ", reflect.TypeOf(received))
+        println(received, "donne : ", string(received))
+        fmt.Print("MatriceR : ", matriceR)
+        fmt.Print(" de type : ", reflect.TypeOf(matriceR))
+
+        //Mettre la réponse dans un fichier text
+
+        file, err := os.Create("matriceR")
+        if err != nil {
+                panic(err)
+        }
+        defer file.Close()
+
+        // Write the matrix contents to the file
+        for i := range matriceR {
+                for j := range matriceR[i] {
+                _, err := file.WriteString(fmt.Sprintf("%d ", matriceR[i][j]))
+                if err != nil {
+                        panic(err)
+                }
+                }
+                _, err := file.WriteString("\n")
+                if err != nil {
+                panic(err)
+                }
+        }
+
 
         conn.Close()
-
 }
