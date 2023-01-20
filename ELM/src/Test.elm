10bfa462bc429d458e0b1b94a68011c6fda1e454
Nitsugu81 <122277644+Nitsugu81@users.noreply.github.com>
2023-01-20 13:34:33 +0100

    Update Test.elm

--- ELM/src/Test.elm
+++ ELM/src/Test.elm
@@ -2,7 +2,7 @@ module Test exposing (..)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (style)
+import Html.Attributes exposing (style, value, type_)
 import Html.Events exposing (..)
 import Http
 import Json.Decode exposing (Decoder, map2, list, field, string)
@@ -22,9 +22,8 @@ main =
 type Model
   = Failure
   | Loading
-  | Success Word
-
-type alias Description = List Word
+  | Success (List Word)
+  | WordMatch String (List Word)
 
 type alias Word =
     { word : String
@@ -47,18 +46,18 @@ init _ =
 -- UPDATE
 
 type Msg
-  = GotWord (Result Http.Error Word)
+  = GotWord (Result Http.Error (List Word))
+  | CheckWord String
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  case msg of
-    GotWord result ->
-      case result of
-        Ok word ->
-          (Success word, Cmd.none)
-
-        Err _ ->
-          (Failure, Cmd.none)
+    case msg of
+        GotWord (Ok words) ->
+            (Success words, Cmd.none)
+        GotWord (Err error) ->
+            (Failure, Cmd.none)
+        CheckWord entered ->
+            (WordMatch entered words, Cmd.none)
 
 -- SUBSCRIPTIONS
 
@@ -73,64 +72,85 @@ view model =
   div []
     [ h2 [] [ text "Word Definitions" ]
     , viewWord model
+    , input [ type_ "text", onInput CheckWord] []
+    , viewWordMatch model
     ]
+
+
 
 viewWord : Model -> Html Msg
 viewWord model =
   case model of
     Failure ->
       div []
-        [ text "I could not load the word for some reason. "
-        ]
+        [ text "I could not load the word for some reason. "        ]
 
     Loading ->
       text "Loading..."
 
-    Success word ->
-      div []
-        [ h3 [] [ text word.word ]
-        , ul [] (List.map viewDef word.meanings)
+    Success words ->
+        div [] (List.map viewWordMeaning words)
+    _ -> text ""
+
+viewWordMeaning : Word -> Html Msg
+viewWordMeaning word =
+    div []
+        [ 
+           ul [] (List.map viewMeaning word.meanings)
         ]
 
-viewDef : Meaning -> Html Msg
-viewDef def =
-  li []
-    [ text def.partOfSpeech
-    , ul [] (List.map viewDefinition def.definitions)
-    ]
+viewMeaning : Meaning -> Html Msg
+viewMeaning meaning =
+    li []
+        [ text meaning.partOfSpeech        , ul [] (List.map viewDefinition meaning.definitions)
+        ]
 
 viewDefinition : Definition -> Html Msg
 viewDefinition def =
-  li [] [ text def.definition ]
+    li [] [ text def.definition ]
+
+viewWordMatch : Model -> Html Msg
+viewWordMatch model =
+  case model of
+    WordMatch entered words ->
+        case words of
+            word::_ ->
+                case (word.word == entered) of
+                    True -> text "You found the word!"
+                    False -> text "Try again!"
+            _ -> text ""
+    _ -> text
+
+
+enteredWord : String
+enteredWord = ""
 
 -- HTTP
 
 getWord : Cmd Msg
 getWord =
-  Http.get
+    Http.get
     { url = "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
-    , expect = Http.expectJson GotWord wordDecoder
+    , expect = Http.expectJson GotWord descriptionDecoder
     }
+
 --Decoder
 descriptionDecoder : Decoder (List Word)
-descriptionDecoder = Json.Decode.list wordDecoder 
+descriptionDecoder = Json.Decode.list wordDecoder
 
 wordDecoder : Decoder Word
-wordDecoder = 
-    map2 Word 
+wordDecoder =
+    map2 Word
         (field "word" string)
-        (field "meanings" <| Json.Decode.list meaningDecoder)
+        (field "meanings" (Json.Decode.list meaningDecoder))
 
 meaningDecoder : Decoder Meaning
-meaningDecoder = 
+meaningDecoder =
     map2 Meaning
         (field "partOfSpeech" string)
-        (field "definitions" <| Json.Decode.list definitionDecoder)
+        (field "definitions" (Json.Decode.list definitionDecoder))
 
 definitionDecoder : Decoder Definition
-definitionDecoder = 
-    Json.Decode.map Definition 
+definitionDecoder =
+    Json.Decode.map Definition
         (field "definition" string)
-
-
-
