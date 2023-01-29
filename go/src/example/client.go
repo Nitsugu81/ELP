b37064c7aa4a8feb43dc10e14054b078f2e1cdcc
Chomosuke123 <122450828+Chomosuke123@users.noreply.github.com>
2023-01-29 15:11:33 +0100

    Add files via upload

--- go/src/example/client.go
+++ go/src/example/client.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-    "io/ioutil"
-    "io"
-    "os"
-    "net"
-    "fmt"
-    "strings"
-    "bufio"
-    "strconv"
-
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"os"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -20,114 +19,114 @@ const (
 
 func main() {
 
-        //PARTIE LECTURE FICHIER//    
+	//PARTIE LECTURE FICHIER//
 
-        //Ouverture MatriceA
-        file1, err := os.Open("MatriceA")
-        if err != nil {
-                panic(err)
-        }
-        defer file1.Close()
+	//Ouverture MatriceA
+	file1, err := os.Open("MatriceA")
+	if err != nil {
+		panic(err)
+	}
+	defer file1.Close()
 
-        //Ouverture MatriceB
-        file2, err := os.Open("MatriceB")
-        if err != nil {
-                panic(err)
-        }
-        defer file2.Close()
+	//Ouverture MatriceB
+	file2, err := os.Open("MatriceB")
+	if err != nil {
+		panic(err)
+	}
+	defer file2.Close()
 
-        // Lecture MatriceA
-        matriceA, err := ioutil.ReadAll(file1)
-        if err != nil {
-                panic(err)
-        }
-        matriceA_lignes := strings.Split(string(matriceA),"\n")
-        nb_lignes_matrice1 := (len(matriceA_lignes))
+	// Lecture MatriceA
+	matriceA, err := ioutil.ReadAll(file1)
+	if err != nil {
+		panic(err)
+	}
+	matriceA_lignes := strings.Split(string(matriceA), "\n")
+	nb_lignes_matrice1 := (len(matriceA_lignes))
 
-        // Lecture MatriceB
-        matriceB, err := ioutil.ReadAll(file2)
-        if err != nil {
-                panic(err)
-        }
-        matriceB_lignes := strings.Split(string(matriceB),"\n")
-        nb_colonnes_matrice2  := (len(strings.Split(matriceB_lignes[0], " ")))
+	// Lecture MatriceB
+	matriceB, err := ioutil.ReadAll(file2)
+	if err != nil {
+		panic(err)
+	}
+	matriceB_lignes := strings.Split(string(matriceB), "\n")
+	nb_colonnes_matrice2 := (len(strings.Split(matriceB_lignes[0], " ")))
 
-        matrices := string(matriceA) + "/////" + string(matriceB)
+	matrices := string(matriceA) + "/////" + string(matriceB)
 
-        //PARTIE ENVOI AU SERVEUR//
+	//PARTIE ENVOI AU SERVEUR//
 
-        tcpServer, err := net.ResolveTCPAddr(TYPE, HOST+":"+PORT)
-        if err != nil {
-                println("ResolveTCPAddr failed:", err.Error())
-                os.Exit(1)
-        }
+	tcpServer, err := net.ResolveTCPAddr(TYPE, HOST+":"+PORT)
+	if err != nil {
+		println("ResolveTCPAddr failed:", err.Error())
+		os.Exit(1)
+	}
 
-        conn, err := net.DialTCP(TYPE, nil, tcpServer) //nil represente l'adresse locale
-        if err != nil {
-                println("Dial failed:", err.Error())
-                os.Exit(1)
-        }
+	conn, err := net.DialTCP(TYPE, nil, tcpServer) //nil represente l'adresse locale
+	if err != nil {
+		println("Dial failed:", err.Error())
+		os.Exit(1)
+	}
 
-        writer := bufio.NewWriter(conn) // Utilisation de bufio pour pouvoir utiliser flush
-        _, err = writer.Write([]byte(matrices))
-        if err != nil {
-                println("Write matrices failed:", err.Error())
-                os.Exit(1)
-        }
-        writer.Flush() //Permet d'envoyer les données directement sans avoir à attendre que le buffer se remplisse ou qu'il s'envoie après un timeout. 
+	writer := bufio.NewWriter(conn) // Utilisation de bufio pour pouvoir utiliser flush
+	_, err = writer.Write([]byte(matrices))
+	if err != nil {
+		println("Write matrices failed:", err.Error())
+		os.Exit(1)
+	}
+	writer.Flush() //Permet d'envoyer les données directement sans avoir à attendre que le buffer se remplisse ou qu'il s'envoie après un timeout.
 
-        //PARTIE RECEP DES DONNEES ENVOYEES PAR LE SERVEUR
+	//PARTIE RECEP DES DONNEES ENVOYEES PAR LE SERVEUR
 
-        reader := bufio.NewReader(conn)
-        matriceR := make([][]int, nb_lignes_matrice1)
-            for i := 0; i < nb_lignes_matrice1; i++ {
-                matriceR[i] = make([]int, nb_colonnes_matrice2)
-            }
-        compteur := 0
-        if err != nil {
-                panic(err)
-        }
-        file, err := os.Create("matriceR")
-        if err != nil {
-                panic(err)
-        }
-        defer file.Close()
-        for {
-                line, err := reader.ReadString('\n')
-                if strings.Contains(line,"Il faut autant de colonnes pour la matriceA que de lignes pour la matriceB\n"){
-                        fmt.Println(line)
-                        break
-                }
-                ligne_matrice := strings.Split(line," ")
-                if strings.Contains(line, "..."){
-                        break
-                }
-                for i,v := range ligne_matrice{ //Utilse si jamais on veut avoir la matrice R dans une variable et pas que dans un fichier text. (si jamais le client veut la remanipuler sans passer par lecture de fichier)
-                        if i != len(ligne_matrice) -1{
-                                matriceR[compteur][i],_ = strconv.Atoi(v)
-                        }else{
-                                matriceR[compteur][i],_ = strconv.Atoi(strings.Split(v,"\n")[0])
-                        }                      
-                }
-                compteur ++ 
-                if err != nil {
-                        if err == io.EOF {
-                                break
-                        }
-                        panic(err)
-                }
-                _, err = file.WriteString(line)
-                if err != nil {
-                        panic(err)
-                }
-        }
+	reader := bufio.NewReader(conn)
+	matriceR := make([][]int, nb_lignes_matrice1)
+	for i := 0; i < nb_lignes_matrice1; i++ {
+		matriceR[i] = make([]int, nb_colonnes_matrice2)
+	}
+	compteur := 0
+	if err != nil {
+		panic(err)
+	}
+	file, err := os.Create("matriceR")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	for {
+		line, err := reader.ReadString('\n')
+		if strings.Contains(line, "Il faut autant de colonnes pour la matriceA que de lignes pour la matriceB\n") {
+			fmt.Println(line)
+			break
+		}
+		ligne_matrice := strings.Split(line, " ")
+		if strings.Contains(line, "...") {
+			break
+		}
+		for i, v := range ligne_matrice { //Utilse si jamais on veut avoir la matrice R dans une variable et pas que dans un fichier text. (si jamais le client veut la remanipuler sans passer par lecture de fichier)
+			if i != len(ligne_matrice)-1 {
+				matriceR[compteur][i], _ = strconv.Atoi(v)
+			} else {
+				matriceR[compteur][i], _ = strconv.Atoi(strings.Split(v, "\n")[0])
+			}
+		}
+		compteur++
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+		_, err = file.WriteString(line)
+		if err != nil {
+			panic(err)
+		}
+	}
 
-        fmt.Println("Matrice R : ")
-        for i := range matriceR {
-                for j := range matriceR[i] {
-                    fmt.Printf("%d ",matriceR[i][j])
-                }
-                fmt.Println()
-            }
- 
+	fmt.Println("Matrice R : ")
+	for i := range matriceR {
+		for j := range matriceR[i] {
+			fmt.Printf("%d ", matriceR[i][j])
+		}
+		fmt.Println()
+	}
+
 }
--- go/src/example/serveur.go
+++ go/src/example/serveur.go
@@ -1,156 +1,168 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "strings"
-    "sync"
-    "reflect"
-    "strconv"
-    "bytes"
-    "io"
+	"bytes"
+	"fmt"
+	"io"
+	"net"
+	"strconv"
+	"strings"
+	"sync"
 )
 
 func main() {
-    // Listen for incoming connections. 
-    addr := "localhost:8888"
-    l, err := net.Listen("tcp", addr)
-    if err != nil {
-        panic(err)
-    }
-    defer l.Close()
-    host, port, err := net.SplitHostPort(l.Addr().String())
-    if err != nil {
-        panic(err)
-    }
-    fmt.Printf("Listening on host: %s, port: %s\n", host, port)
+	// Listen for incoming connections.
+	addr := "localhost:8888"
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+	host, port, err := net.SplitHostPort(l.Addr().String())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Listening on host: %s, port: %s\n", host, port)
 
-    for {
-        // Listen for an incoming connection
+	for {
+		// Listen for an incoming connection
 
-        conn, err := l.Accept()
-        if err != nil {
-            panic(err)
-        }
-        go func(conn net.Conn) {
+		conn, err := l.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go func(conn net.Conn) {
 
-            buf := bytes.NewBuffer(nil)
-            delimiter := []byte("...")
-            for {
-                tmp := make([]byte, 256) //De petite taille pour pas trop lire (en gros on lit 256 bytes par 256 bytes et on rajoute ca dans un buffer, comme ca ca evite de faire un buffer trop gros ou trop petit)
-                n, err := conn.Read(tmp)
-                if err != nil {
-                    if err == io.EOF {
-                        break
-                    }
-                    fmt.Printf("Error reading: %#v\n", err)
-                    return
-                }
-                buf.Write(tmp[:n])
-                if bytes.Contains(buf.Bytes(), delimiter) {
-                    break
-                }
-            }
-            matrices := buf.String()
+			buf := bytes.NewBuffer(nil)
+			delimiter := []byte("...")
+			for {
+				tmp := make([]byte, 256) //De petite taille pour pas trop lire (en gros on lit 256 bytes par 256 bytes et on rajoute ca dans un buffer, comme ca ca evite de faire un buffer trop gros ou trop petit)
+				n, err := conn.Read(tmp)
+				if err != nil {
+					if err == io.EOF {
+						break
+					}
+					fmt.Printf("Error reading: %#v\n", err)
+					return
+				}
+				buf.Write(tmp[:n])
+				if bytes.Contains(buf.Bytes(), delimiter) {
+					break
+				}
+			}
+			matrices := buf.String()
+			matrices_slices := strings.Split(matrices, "/////")
+			matriceA := matrices_slices[0]
+			matriceB := matrices_slices[1]
+			matriceA_lignes := strings.Split(matriceA, "\n")
+			matriceB_lignes := strings.Split(matriceB, "\n")
+			matriceB_lignes = matriceB_lignes[:len(matriceB_lignes)-1]
 
-            //matrices := string(buf[:taille])
-            matrices_slices := strings.Split(matrices,"/////")
-            matriceA := matrices_slices[0]
-            matriceB := matrices_slices[1]
-            matriceA_lignes := strings.Split(matriceA,"\n")
-            matriceB_lignes := strings.Split(matriceB,"\n")
-            matriceB_lignes = matriceB_lignes[:len(matriceB_lignes)-1]
+			nb_lignes_matrice1 := (len(matriceA_lignes))
+			nb_lignes_matrice2 := (len(matriceB_lignes))
+			nb_colonnes_matrice1 := (len(strings.Split(matriceA_lignes[0], " ")))
+			nb_colonnes_matrice2 := (len(strings.Split(matriceB_lignes[0], " ")))
 
-            nb_lignes_matrice1 := (len(matriceA_lignes))
-            nb_lignes_matrice2 := (len(matriceB_lignes))
-            nb_colonnes_matrice1 := (len(strings.Split(matriceA_lignes[0], " ")))
-            nb_colonnes_matrice2  := (len(strings.Split(matriceB_lignes[0], " ")))
+			matrice1 := make([][]int, nb_lignes_matrice1)
+			for i := 0; i < nb_lignes_matrice1; i++ {
+				matrice1[i] = make([]int, nb_colonnes_matrice1)
+			} //On pouvait pas passer par des arrays parce que la taille est pas constante (depend des matrices envoyées) donc ca ne marchait pas
+			matrice2 := make([][]int, nb_lignes_matrice2)
+			for i := 0; i < nb_lignes_matrice2; i++ {
+				matrice2[i] = make([]int, nb_colonnes_matrice2)
+			}
+			matriceR := make([][]int, nb_lignes_matrice1)
+			for i := 0; i < nb_lignes_matrice1; i++ {
+				matriceR[i] = make([]int, nb_colonnes_matrice2)
+			}
 
-            fmt.Print("nb_lignes : ", nb_lignes_matrice1)
-            fmt.Println(" de type : ", reflect.TypeOf(nb_lignes_matrice1))
-            //tata := 2
-            matrice1 := make([][]int, nb_lignes_matrice1)
-            for i := 0; i < nb_lignes_matrice1; i++ {
-                matrice1[i] = make([]int, nb_colonnes_matrice1)
-            } //On pouvait pas passer par des arrays parce que la taille est pas constante (depend des matrices envoyées) donc ca ne marchait pas
-            matrice2 := make([][]int, nb_lignes_matrice2)
-            for i := 0; i < nb_lignes_matrice2; i++ {
-                matrice2[i] = make([]int, nb_colonnes_matrice2)
-            }
-            matriceR := make([][]int, nb_lignes_matrice1)
-            for i := 0; i < nb_lignes_matrice1; i++ {
-                matriceR[i] = make([]int, nb_colonnes_matrice2)
-            }
+			if nb_colonnes_matrice1 != nb_lignes_matrice2 {
+				conn.Write([]byte("Il faut autant de colonnes pour la matriceA que de lignes pour la matriceB\n"))
+				conn.Close()
+				return
+			}
 
-            if nb_colonnes_matrice1 != nb_lignes_matrice2{
-                conn.Write([]byte("Il faut autant de colonnes pour la matriceA que de lignes pour la matriceB\n"))
-                conn.Close()
-                return 
-            }
+			for i := 0; i < nb_lignes_matrice1; i++ { //Si jamais le user fait des lignes avec des nombres de colonne differentes (pas opti car 2 boucles for mais ca fait crash le serveur sinon)
+				temp := len(strings.Split(matriceA_lignes[i], " "))
+				if temp != nb_colonnes_matrice1 {
+					conn.Write([]byte("Il faut autant de colonnes pour la matriceA que de lignes pour la matriceB\n"))
+					conn.Close()
+					return
+				}
+			}
 
-            for i := 0; i < nb_lignes_matrice1; i++ { //ATTENTION, UN STRING C EST UNE CHAINE DE BYTE ET NON PAS UNE CHAINE DE CHARACTERES (Histoire avec rune)
-                matriceA_elems := strings.Split(matriceA_lignes[i], " ")
-                for j := 0; j < nb_colonnes_matrice1; j++{
-                    matrice1[i][j],err = strconv.Atoi((string(matriceA_elems[j])))
-                    if err != nil {
-                        fmt.Print("ERREUR")
-                    }
-                }
-                    
-            }
+			for i := 0; i < nb_lignes_matrice2; i++ {
+				temp := len(strings.Split(matriceB_lignes[i], " "))
+				if temp != nb_colonnes_matrice2 {
+					conn.Write([]byte("Il faut autant de colonnes pour la matriceA que de lignes pour la matriceB\n"))
+					conn.Close()
+					return
+				}
+			}
 
-            fmt.Println("\nMatrice A : ")
+			for i := 0; i < nb_lignes_matrice1; i++ { //ATTENTION, UN STRING C EST UNE CHAINE DE BYTE ET NON PAS UNE CHAINE DE CHARACTERES (Histoire avec rune)
+				matriceA_elems := strings.Split(matriceA_lignes[i], " ")
+				for j := 0; j < nb_colonnes_matrice1; j++ {
+					matrice1[i][j], err = strconv.Atoi((string(matriceA_elems[j])))
+					if err != nil {
+						fmt.Print("ERREUR")
+					}
+				}
 
-            for i := range matrice1 {
-                for j := range matrice1[i] {
-                    fmt.Printf("%d ",matrice1[i][j])
-                }
-                fmt.Println()
-            }
-            
-            fmt.Println("Matrice B : ")
+			}
 
-            for i := 0; i < nb_lignes_matrice2; i++ { 
-                matriceB_elems := strings.Split(matriceB_lignes[i], " ")
-                for j := 0; j < nb_colonnes_matrice2; j++{
-                    matrice2[i][j],_ = strconv.Atoi((string(matriceB_elems[j])))
-                }
-                    
-            }
-            
-             for i := range matrice2 {
-                for j := range matrice2[i] {
-                    fmt.Printf("%d ",matrice2[i][j])
-                }
-                fmt.Println()
-            }
+			fmt.Println("\nMatrice A : ")
 
-            var wg sync.WaitGroup
-            for i := range matrice1 {
-                wg.Add(1)
-                go multiply(matrice1, matrice2, &matriceR, i, &wg)
-            }
-            wg.Wait()
-            for i := range matriceR{
-                for j:= range matriceR[i]{
-                    conn.Write([]byte(strconv.Itoa(matriceR[i][j])))
-                    if j != len(matriceR[i]) - 1{
-                        conn.Write([]byte(" "))
-                    }
-                }
-                conn.Write([]byte("\n"))
-            }
-            conn.Write([]byte("...\n"))
-            conn.Close()
-        }(conn)
-    }
+			for i := range matrice1 {
+				for j := range matrice1[i] {
+					fmt.Printf("%d ", matrice1[i][j])
+				}
+				fmt.Println()
+			}
+
+			fmt.Println("Matrice B : ")
+
+			for i := 0; i < nb_lignes_matrice2; i++ {
+				matriceB_elems := strings.Split(matriceB_lignes[i], " ")
+				for j := 0; j < nb_colonnes_matrice2; j++ {
+					matrice2[i][j], _ = strconv.Atoi((string(matriceB_elems[j])))
+				}
+
+			}
+
+			for i := range matrice2 {
+				for j := range matrice2[i] {
+					fmt.Printf("%d ", matrice2[i][j])
+				}
+				fmt.Println()
+			}
+
+			var wg sync.WaitGroup
+			for i := range matrice1 {
+				wg.Add(1)
+				go multiply(matrice1, matrice2, &matriceR, i, &wg)
+			}
+			wg.Wait()
+			for i := range matriceR {
+				for j := range matriceR[i] {
+					conn.Write([]byte(strconv.Itoa(matriceR[i][j])))
+					if j != len(matriceR[i])-1 {
+						conn.Write([]byte(" "))
+					}
+				}
+				conn.Write([]byte("\n"))
+			}
+			conn.Write([]byte("...\n"))
+			conn.Close() //flush integré
+		}(conn)
+	}
 }
 
 func multiply(a [][]int, b [][]int, result *[][]int, row int, wg *sync.WaitGroup) {
-    defer wg.Done()
-    for col := 0; col < len(b[0]); col++ {
-        for k := 0; k < len(a[0]); k++ {
-            (*result)[row][col] += a[row][k] * b[k][col]
-        }
-    }
+	defer wg.Done()
+	for col := 0; col < len(b[0]); col++ {
+		for k := 0; k < len(a[0]); k++ {
+			(*result)[row][col] += a[row][k] * b[k][col]
+		}
+	}
 }
